4cd68838fd3f2aa5d676431d36a9c17e4f1f14a8
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-02-19 14:58:10 -0300

    Notas coletas devem aparecer tambem no painel sem coleta

--- src/main/kotlin/br/com/astrosoft/devolucao/view/devolucao/DevolucaoView.kt
+++ src/main/kotlin/br/com/astrosoft/devolucao/view/devolucao/DevolucaoView.kt
@@ -18,10 +18,10 @@ class DevolucaoView: ViewLayout<DevolucaoViewModel>(), IDevolucaoView {
   override val tabPedido = TabPedido(viewModel.tabPedidoViewModel)
   override val tabEntrada = TabEntrada(viewModel.tabEntradaViewModel)
   override val tabNotaSerie66 = TabNotaSerie66(viewModel.tabNotaDevolucaoViewModel)
-  override val tabNotaSerie66Pago = TabNotaSerie66Pago(viewModel.tabNota66Pago)
+  override val tabNotaSerie66Pago = TabNotaSerie66Pago(viewModel.tabNota66PagoViewModel)
   override val tabNotaSerie01 = TabNotaSerie01(viewModel.tabNotaSerie01ViewModel)
   override val tabNotaSerie01Coleta = TabNotaSerie01Coleta(viewModel.tabNotaSerie01ColetaViewModel)
-  override val tabEmailRecebido = TabEmailRecebido(viewModel.tabEmailRecebido)
+  override val tabEmailRecebido = TabEmailRecebido(viewModel.tabEmailRecebidoViewModel)
   
   override fun isAccept(user: IUser): Boolean {
     val userSaci = user as? UserSaci ?: return false
