6d3f2246f210808cfd33c9f55d72624033da32ad
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-02-23 03:42:01 -0300

    refatoramento

--- src/main/kotlin/br/com/astrosoft/framework/spring/Application.kt
+++ src/main/kotlin/br/com/astrosoft/framework/spring/Application.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-@SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
+@SpringBootApplication//(exclude = [ErrorMvcAutoConfiguration::class])
 class Application: SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
