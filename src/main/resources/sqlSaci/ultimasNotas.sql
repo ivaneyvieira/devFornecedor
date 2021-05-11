f835def3c4f2d154a44ce241697d1e909585fa2f
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-05-11 10:34:45 -0300

    Otimização na hora de gerar a planilha

--- src/main/resources/sqlSaci/ultimasNotas.sql
+++ src/main/resources/sqlSaci/ultimasNotas.sql
@@ -1,19 +1,10 @@
-SELECT I.invno                                      AS ni,
-       I.storeno                                    AS loja,
-       CAST(CONCAT(I.nfname, '/', I.invse) AS CHAR) AS nfNumero,
-       CAST(I.issue_date AS DATE)                   AS data,
-       TRIM(P.prdno)                                AS codigo,
-       P.grade                                      AS grade,
-       P.qtty / 1000                                AS qttd,
-       vendno                                       AS vendno,
-       IFNULL(R.form_label, '')                     AS rotulo
-FROM sqldados.inv            AS I
-  INNER JOIN sqldados.iprd   AS P
-	       USING (invno)
-  LEFT JOIN  sqldados.prdalq AS R
-	       USING (prdno)
-WHERE I.date > 20150101
-  AND I.type = 0
-  AND I.storeno IN (2, 3, 4, 5)
-  AND I.bits & POW(2, 4) = 0
-  AND I.auxShort13 & POW(2, 15) = 0
+SELECT ni,
+       loja,
+       nfNumero,
+       data,
+       codigo,
+       grade,
+       qttd,
+       vendno,
+       rotulo
+FROM sqldados.ultimascompras
