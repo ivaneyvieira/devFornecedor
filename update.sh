774624c35c72a46c8234468701905ff20b9f6ff9
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-05-23 10:11:00 -0300

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git pull
-./gradlew clean vaadinBuildFrontend build -Pvaadin.productionMode
+./gradlew clean build -Pvaadin.productionMode
 
 docker-compose down
 docker-compose up -d
