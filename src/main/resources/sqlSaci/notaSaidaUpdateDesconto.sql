e1192d21e7e87e5b9099971dc7dc3a818c39e843
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-10-08 10:43:23 -0300

    Agora está slavando as situações

--- src/main/resources/sqlSaci/notaSaidaUpdateDesconto.sql
+++ src/main/resources/sqlSaci/notaSaidaUpdateDesconto.sql
@@ -1,8 +1,7 @@
-observacaoAuxiliar
-
 UPDATE sqldados.nf
 SET c6  = MID(TRIM(IFNULL(:chaveDesconto, '')), 1, 40),
     c5  = MID(TRIM(IFNULL(:chaveDesconto, '')), 41, 40),
+    c4  = :observacaoAuxiliar,
     l15 = :dataAgenda
 WHERE storeno = :loja
   AND pdvno = :pdv
