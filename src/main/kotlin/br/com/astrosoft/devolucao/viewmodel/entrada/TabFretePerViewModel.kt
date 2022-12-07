b23972211dcd436d881780ed6cf662264f23e259
Ivaney Sales <ivaneyvieira@gmail.com>
2022-12-07 09:53:02 -0300

    Copia viewmodel frete per

---
+++ src/main/kotlin/br/com/astrosoft/devolucao/viewmodel/entrada/TabFretePerViewModel.kt
@@ -0,0 +1,62 @@
+package br.com.astrosoft.devolucao.viewmodel.entrada
+
+import br.com.astrosoft.devolucao.model.beans.FiltroRelatorio
+import br.com.astrosoft.devolucao.model.beans.Loja
+import br.com.astrosoft.devolucao.model.beans.NfPrecEntrada
+import br.com.astrosoft.devolucao.model.beans.NfPrecEntradaGrupo
+import br.com.astrosoft.devolucao.model.planilhas.PlanilhaFreteDif
+import br.com.astrosoft.devolucao.model.reports.RelatorioFreteDif
+import br.com.astrosoft.devolucao.model.reports.RelatorioNfPrecGrupo
+import br.com.astrosoft.devolucao.model.saci
+import br.com.astrosoft.framework.util.format
+import br.com.astrosoft.framework.viewmodel.ITabView
+import br.com.astrosoft.framework.viewmodel.fail
+
+class TabFretePerViewModel(val viewModel: EntradaViewModel) {
+  val subView
+    get() = viewModel.view.tabFretePerViewModel
+
+  fun openDlgRelatorio() = viewModel.exec {
+    saci.queryNfPrec(subView.getFiltro())
+    subView.openRelatorio()
+  }
+
+  fun imprimeRelatorio(listNotas: List<NfPrecEntrada>) = viewModel.exec {
+    if (listNotas.isEmpty()) fail("Nenhuma nota selecionada")
+    val relatorio = RelatorioFreteDif.processaRelatorio(listNotas)
+    viewModel.showReport("nfPrecificacao", relatorio)
+  }
+
+  fun imprimeRelatorioResumo(listNotas: List<NfPrecEntrada>) {
+    val listaRelatorio = listNotas.filter { it.freteDif != "S" }.map { nota ->
+      NfPrecEntradaGrupo(nomeGrupo = "Diferenças de Frete",
+                         nota = nota,
+                         pedidoCompra = nota.pedidoCompra ?: 0,
+                         valorNota = nota.freten.format(),
+                         valorPrecificacao = nota.fretep.format())
+    }
+    val relatorio = RelatorioNfPrecGrupo.processaRelatorio(listaRelatorio, fiscal = true)
+    viewModel.showReport("nfPrecificacaoGrupo", relatorio)
+  }
+
+  fun geraPlanilha(notas: List<NfPrecEntrada>): ByteArray {
+    val planilha = PlanilhaFreteDif()
+    return planilha.grava(notas)
+  }
+
+  fun findLojas(): List<Loja> {
+    return Loja.allLojas().sortedBy { it.no }
+  }
+
+  fun findNotas(filtro: FiltroRelatorio): List<NfPrecEntrada> {
+    return NfPrecEntrada.findNotas(filtro).filter {
+      it.frete != 0.00 && it.fretep != 0.00 && it.freten != 0.00
+    }
+  }
+}
+
+interface ITabFretePerViewModel : ITabView {
+  fun setFiltro(filtro: FiltroRelatorio)
+  fun getFiltro(): FiltroRelatorio
+  fun openRelatorio()
+}
