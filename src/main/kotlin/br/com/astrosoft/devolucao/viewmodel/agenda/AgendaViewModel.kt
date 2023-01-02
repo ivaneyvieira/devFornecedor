66951fec40ed29d1fc7ff1c54466f2f6e5755c9b
Ivaney Sales <ivaneyvieira@gmail.com>
2023-01-02 02:06:45 -0300

    Ajuste view model para o painel rastreamento

--- src/main/kotlin/br/com/astrosoft/devolucao/viewmodel/agenda/AgendaViewModel.kt
+++ src/main/kotlin/br/com/astrosoft/devolucao/viewmodel/agenda/AgendaViewModel.kt
@@ -7,12 +7,15 @@ class AgendaViewModel(view: IAgendaView) : ViewModel<IAgendaView>(view) {
   val tabAgendadaVMAgendada = TabAgendaViewModelAgendada(this)
   val tabAgendadaVMNaoAgendada = TabAgendaViewModelNaoAgendada(this)
   val tabAgendadaVMRecebida = TabAgendaViewModelRecebida(this)
+  val tabAgendadaRastreamento = TabAgendaRastreamentoViewModel(this)
 
-  override fun listTab() = listOf(view.tabAgendaNaoAgendada, view.tabAgendaAgendada, view.tabAgendaRecebida)
+  override fun listTab() =
+    listOf(view.tabAgendaNaoAgendada, view.tabAgendaRastreamento, view.tabAgendaAgendada, view.tabAgendaRecebida)
 }
 
 interface IAgendaView : IView {
   val tabAgendaRecebida: ITabAgendaRecebida
   val tabAgendaAgendada: ITabAgendaAgendada
   val tabAgendaNaoAgendada: ITabAgendaNaoAgendada
+  val tabAgendaRastreamento: ITabAgendaRastreamento
 }
