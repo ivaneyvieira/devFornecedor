b7928f8307c6f859d3917668a0f5992d5df5d8fc
Ivaney Sales <ivaneyvieira@gmail.com>
2023-01-11 06:10:07 -0300

    Crud da tabela Agenda Demanda

--- src/main/kotlin/br/com/astrosoft/devolucao/model/beans/AgendaDemanda.kt
+++ src/main/kotlin/br/com/astrosoft/devolucao/model/beans/AgendaDemanda.kt
@@ -1,10 +1,30 @@
 package br.com.astrosoft.devolucao.model.beans
 
+import br.com.astrosoft.devolucao.model.saci
 import java.time.LocalDate
 
 class AgendaDemanda(
-  val id: Int,
+  val id: Int = 0,
   val date: LocalDate,
   val titulo: String,
   val conteudo: String,
-                   )
+                   ){
+  fun delete() = saci.deleteAgendaDemanda(this)
+
+  fun save() {
+    if(id == 0)
+      insert()
+    else
+      update()
+  }
+
+  fun insert() = saci.insertAgendaDemanda(this)
+
+  fun update() = saci.updateAgendaDemanda(this)
+
+  companion object {
+    fun findAll() : List<AgendaDemanda> {
+      return saci.selectAgendaDemanda()
+    }
+  }
+}
