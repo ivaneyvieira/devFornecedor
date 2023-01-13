c587cd39576352883c98e64e7869a0fea92940cf
Ivaney Sales <ivaneyvieira@gmail.com>
2023-01-13 08:48:17 -0300

    Crud para os arquivos anexados da agenda de demanda

--- src/main/kotlin/br/com/astrosoft/devolucao/model/beans/AgendaDemanda.kt
+++ src/main/kotlin/br/com/astrosoft/devolucao/model/beans/AgendaDemanda.kt
@@ -8,22 +8,34 @@ class AgendaDemanda(
   var date: LocalDate,
   var titulo: String,
   var conteudo: String,
-                   ){
+                   ) {
   fun delete() = saci.deleteAgendaDemanda(this)
 
   fun save() {
-    if(id == 0)
-      insert()
-    else
-      update()
+    if (id == 0) insert()
+    else update()
   }
 
   fun insert() = saci.insertAgendaDemanda(this)
 
   fun update() = saci.updateAgendaDemanda(this)
 
+  fun findAnexos(): List<NFFile> {
+    return saci.selectFile(this)
+  }
+
+  fun addAnexo(nome: String, file: ByteArray) {
+    val nfFile = NFFile(storeno = 1, pdvno = 8888, xano = id, date = date, nome = nome, file = file)
+    saci.insertFile(nfFile)
+  }
+
+  fun delAnexo(nfFile: NFFile) {
+    saci.deleteFile(nfFile)
+  }
+
+
   companion object {
-    fun findAll() : List<AgendaDemanda> {
+    fun findAll(): List<AgendaDemanda> {
       return saci.selectAgendaDemanda()
     }
   }
