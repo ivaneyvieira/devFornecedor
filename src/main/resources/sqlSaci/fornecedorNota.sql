de3029dbfa20a5fd46dd09bd4088a3ffa6c11466
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-03-13 10:25:55 -0300

    Atualizações das colunas referente as parcelas das notas fisicais

--- src/main/resources/sqlSaci/fornecedorNota.sql
+++ src/main/resources/sqlSaci/fornecedorNota.sql
@@ -4,16 +4,63 @@ SET @VENDNO := :vendno;
 SET @QUERY := :query;
 SET @QUERY_LIKE := CONCAT('%', @QUERY, '%');
 
+
+DROP TEMPORARY TABLE IF EXISTS T_PARCELA_ULT;
+CREATE TEMPORARY TABLE T_PARCELA_ULT (
+  PRIMARY KEY (invno),
+  chave varchar(20)
+)
+SELECT invno, MAX(CONCAT(LPAD(instno, 10, ' '), LPAD(duedate, 10, ' '))) AS chave
+FROM sqldados.invxa       AS X
+  INNER JOIN sqldados.inv AS I
+	       USING (invno)
+WHERE I.vendno = @VENDNO
+GROUP BY invno;
+
+DROP TEMPORARY TABLE IF EXISTS T_PARCELA_ULT2;
+CREATE TEMPORARY TABLE T_PARCELA_ULT2 (
+  PRIMARY KEY (invno, instno, duedate)
+)
+SELECT invno,
+       TRIM(MID(chave, 1, 10)) * 1  AS instno,
+       TRIM(MID(chave, 11, 10)) * 1 AS duedate
+FROM T_PARCELA_ULT;
+
 DROP TEMPORARY TABLE IF EXISTS T_NOTAS;
 CREATE TEMPORARY TABLE T_NOTAS
-SELECT storeno                                  AS loja,
+SELECT I.storeno                                AS loja,
        invno                                    AS ni,
        CAST(CONCAT(nfname, '/', invse) AS char) AS nf,
        CAST(issue_date AS DATE)                 AS emissao,
        CAST(date AS DATE)                       AS entrada,
        grossamt / 100                           AS valorNota,
-       remarks                                  AS obs
-FROM sqldados.inv AS I
+       I.remarks                                AS obs,
+       CAST(X.duedate AS date)                  AS vencimento,
+       CASE X.status
+	 WHEN 0
+	   THEN 'Em Aberto'
+	 WHEN 1
+	   THEN 'Pgto.Total'
+	 WHEN 2
+	   THEN 'Pgto.Parcial'
+	 WHEN 3
+	   THEN 'Pgto.c/Desconto'
+	 WHEN 4
+	   THEN 'Pgto.+ Juros'
+	 WHEN 5
+	   THEN 'Cancelada'
+	 WHEN 6
+	   THEN 'Pgto.c/Devolucao'
+	 WHEN 7
+	   THEN 'Consignacao'
+	 ELSE ''
+       END                                      AS situacao,
+       X.remarks                                AS obsParcela
+FROM sqldados.inv          AS I
+  LEFT JOIN T_PARCELA_ULT2 AS U
+	      USING (invno)
+  LEFT JOIN sqldados.invxa AS X
+	      USING (invno, instno, duedate)
 WHERE vendno = @VENDNO
 ORDER BY invno DESC;
 
